e266a4858c740cab65145bf36f75f2f165d869bc
Saketh Reddy <Sakethreddy1998@gmail.com>
2022-08-29 12:59:18 +0530

    added new commits

---
+++ data.js
@@ -0,0 +1,76 @@
+let songs =[
+
+    {
+        name:'Sarkarvari Patta',
+        path:'assets/music/song 1.mp3',
+        artist:'S S Thaman',
+        cover:'assets/images/cover 1.png'
+    },
+
+    {
+        name:'Naatu Naatu',
+        path:'assets/music/song 2.mp3',
+        artist:'M M Keeravani',
+        cover:'assets/images/cover 2.png'
+    },
+
+    {
+        name:'Sulthan',
+        path:'assets/music/song 3.mp3',
+        artist:'Ravi Basrur',
+        cover:'assets/images/cover 3.png'
+    },
+
+    {
+        name:'Mama Mahesha',
+        path:'assets/music/song 4.mp3',
+        artist:'S S Thaman',
+        cover:'assets/images/cover 4.png'
+    },
+
+    {
+        name:'Kalaavathi',
+        path:'assets/music/song 5.mp3',
+        artist:'S S Thaman',
+        cover:'assets/images/cover 5.png'
+    },
+
+    {
+        name:'Dosti',
+        path:'assets/music/song 6.mp3',
+        artist:'M M keeravani',
+        cover:'assets/images/cover 6.png'
+    },
+
+    {
+        name:'Etthara Jenda',
+        path:'assets/music/song 7.mp3',
+        artist:'M M keeravani',
+        cover:'assets/images/cover 7.png'
+    },
+    {
+        name:'Levitating',
+        path:'assets/music/song 8.mp3',
+        artist:'Dua Lipa',
+        cover:'assets/images/cover 8.png'
+    },
+    {
+        name:'Halmidi Habibi',
+        path:'assets/music/song 9.mp3',
+        artist:'Anirudh',
+        cover:'assets/images/cover 9.jpeg'
+    },
+    {
+        name:'Eyy Bidda Idhi Naa Adda',
+        path:'assets/music/song 10.mp3',
+        artist:'D S P',
+        cover:'assets/images/cover 10.png'
+    },
+    {
+        name:'Oo Antava',
+        path:'assets/music/song 11.mp3',
+        artist:'D S P',
+        cover:'assets/images/cover 11.png'
+    }
+    
+]
